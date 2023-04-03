34394ea8c6387c38686208188299a3dd7b22b3e2
Razi Ahmad <mail2razi4u@gmail.com>
2023-04-04 00:17:24 +0530

    common form retrieve

--- application/views/includes/order_form.php
+++ application/views/includes/order_form.php
@@ -1,4 +1,4 @@
-
+<!-- 
 <style type="text/css">
 .modal-backdrop{
    display: none; 
@@ -203,7 +203,7 @@
                                 <div class="form-group">
                                     <div class="checkbox" style="float: right;">
                                         <label>
-                                            <input type="checkbox" name="to_customer" value="1"> For Student
+                                            <input type="checkbox" name="to_customer" value="1"> For Students
                                         </label>
                                     </div>
                                 </div>
@@ -409,4 +409,31 @@
 
 
 
+ -->
 
+
+
+
+
+
+ <form action="<?php echo base_url();?>order/" method="post" enctype="multipart/form-data" id="orderform">
+                    <input type="hidden" name="<?php echo $this->security->get_csrf_token_name();?>" value="<?php echo $this->security->get_csrf_hash();?>" />  
+							<div class="form_innBox">
+								<div class="form_fields">
+									<textarea class="textarea" name="Details" placeholder="Type your question here" required></textarea>
+								</div>
+								<div class="form_fields">
+									<div class="upload_MFile">
+										<span class="fileinput-button" id="fileinput-button">
+								            <span><i class="fas fa-cloud-upload-alt" aria-hidden="true"></i><br><span>Drop your file or Browse</span> </span>
+								            <input type="file" name="files" id="files" multiple accept="image/jpeg, image/png, image/gif," required><br />
+								        </span>
+								        <output id="Filelist"></output>
+								    </div>
+								</div>
+							</div>
+							<div class="text-center">
+								<input type="submit" class="r_btn" value="Submit" > 
+								<p class="p_text pt-3">Your desired grade is just a click away!</p>
+							</div>
+	</form>
