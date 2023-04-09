ca549605ea193dbf29efd957662b37fd95484465
Razi Ahmad <mail2razi4u@gmail.com>
2023-04-09 13:27:02 +0530

    student dashboard files added

--- application/views/users/head.php
+++ application/views/users/head.php
@@ -8,17 +8,17 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdn.tutorialjinni.com/intl-tel-input/17.0.19/css/intlTelInput.css" />
-	<link href="<?php echo base_url();?>assets/css/dashboard.css" rel="stylesheet">
-	<link href="<?php echo base_url();?>assets/css/chat.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>assets/main/css/dashboard.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>assets/main/css/chat.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&amp;display=swap"
 		rel="stylesheet">
-	<link rel="shortcut icon" href="<?php echo base_url();?>assets/img/fav.png" type="image/x-icon">
+	<link rel="shortcut icon" href="<?php echo base_url();?>assets/main/images/fav.png" type="image/x-icon">
 	<style>
 	     #loading{
         position: fixed;
         width: 100%;
         height: 100vh;
-        background: #fff url('<?php echo base_url();?>assets/front/images/loader.gif') no-repeat center  ;
+        background: #fff url('<?php echo base_url();?>assets/main/images/loader.gif') no-repeat center  ;
         z-index: 999;
         display: none;
         opacity: 0.7;
@@ -32,7 +32,7 @@
 		<div class="container">
 			<div class="row" style="align-items: center;">
 				<div class="col-6">
-					<div><img class="logo" src="<?php echo base_url();?>assets/img/logo.png" alt="logo"></div>
+					<div><img class="logo" src="<?php echo base_url();?>assets/main/images/logo.png" alt="logo"></div>
 				</div>
 				<div class="col-6">
 					<div class="right_links_con">
@@ -40,22 +40,22 @@
 							<li>
 								<a class="cstmBtn create_new_order" href="#">Create New Order</a>
 								<ul class="order_drop_box">
-								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/img/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/img/icon_6_y.png"></div> Homework Assignment Help</a></li>
-								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/img/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/img/icon_6_y.png"></div> Live Session</a></li>
-								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/img/LabReports.png"> <img class="hover_img" src="<?php echo base_url();?>assets/img/LabReports_y.png"></div> Lab Work</a></li>
-								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/img/writing.png"> <img class="hover_img" src="<?php echo base_url();?>assets/img/writing_y.png"></div> The art of essay Writting</a></li>
-								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/img/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/img/icon_6_y.png"></div> Video Solution</a></li>
-								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/img/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/img/icon_6_y.png"></div> Project Work</a></li>
-								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/img/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/img/icon_6_y.png"></div> Speech Writing</a></li>
-								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/img/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/img/icon_6_y.png"></div> Presentation Writing</a></li>
+								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/main/images/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/main/images/icon_6_y.png"></div> Homework Assignment Help</a></li>
+								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/main/images/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/main/images/icon_6_y.png"></div> Live Session</a></li>
+								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/main/images/LabReports.png"> <img class="hover_img" src="<?php echo base_url();?>assets/main/images/LabReports_y.png"></div> Lab Work</a></li>
+								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/main/images/writing.png"> <img class="hover_img" src="<?php echo base_url();?>assets/main/images/writing_y.png"></div> The art of essay Writting</a></li>
+								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/main/images/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/main/images/icon_6_y.png"></div> Video Solution</a></li>
+								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/main/images/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/main/images/icon_6_y.png"></div> Project Work</a></li>
+								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/main/images/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/main/images/icon_6_y.png"></div> Speech Writing</a></li>
+								  <li><a href="#" data-toggle="modal" data-target="#openPopup"><div><img class="deflt_img" src="<?php echo base_url();?>assets/main/images/icon_6.png"> <img class="hover_img" src="<?php echo base_url();?>assets/main/images/icon_6_y.png"></div> Presentation Writing</a></li>
 								</ul>
 							</li>
 							<li>
 								<a class="notiF" href="#"><i class="fa fa-bell" aria-hidden="true"></i> <span class="notiF_count">2</span> </a>
 								<ul class="notiF_drop_box">
-								  <li><a href="#"><div><img src="<?php echo base_url();?>assets/img/accurate-solutions.png"></div> <div class="noti_txt"><span>Order 178132 updated</span><span>Updated successfully</span> <span class="notif_time">2 day ago</span></div> </a></li>
-								  <li><a href="#"><div><img src="<?php echo base_url();?>assets/img/accurate-solutions.png"></div> <div class="noti_txt"><span>Order 178132 updated</span><span>Updated successfully</span> <span class="notif_time">2 day ago</span></div> </a></li>
-								  <li><a href="#"><div><img src="<?php echo base_url();?>assets/img/accurate-solutions.png"></div> <div class="noti_txt"><span>Order 178132 updated</span><span>Updated successfully</span> <span class="notif_time">2 day ago</span></div> </a></li>
+								  <li><a href="#"><div><img src="<?php echo base_url();?>assets/main/images/accurate-solutions.png"></div> <div class="noti_txt"><span>Order 178132 updated</span><span>Updated successfully</span> <span class="notif_time">2 day ago</span></div> </a></li>
+								  <li><a href="#"><div><img src="<?php echo base_url();?>assets/main/images/accurate-solutions.png"></div> <div class="noti_txt"><span>Order 178132 updated</span><span>Updated successfully</span> <span class="notif_time">2 day ago</span></div> </a></li>
+								  <li><a href="#"><div><img src="<?php echo base_url();?>assets/main/images/accurate-solutions.png"></div> <div class="noti_txt"><span>Order 178132 updated</span><span>Updated successfully</span> <span class="notif_time">2 day ago</span></div> </a></li>
 								</ul>
 							</li>
 							<li>
