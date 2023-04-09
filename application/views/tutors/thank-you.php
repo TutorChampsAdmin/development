1a6a44280d853ad6e5eadb1e4cf3dcc217e855c9
Razi Ahmad <mail2razi4u@gmail.com>
2023-04-09 12:59:33 +0530

    tutors files changes

--- application/views/tutors/thank-you.php
+++ application/views/tutors/thank-you.php
@@ -5,7 +5,7 @@
 <title>Thank you - TutorChamps.com</title>
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&amp;display=swap" rel="stylesheet">
 <link rel="shortcut icon" href="<?php echo base_url();?>assets/front/images/fav.png" type="image/x-icon">
-<link rel="stylesheet" href="<?php echo base_url();?>assets/front/css/style.css">
+<link rel="stylesheet" href="<?php echo base_url();?>assets/tutors/css/style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <style>
