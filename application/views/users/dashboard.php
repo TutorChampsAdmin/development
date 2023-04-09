deab05a1792c88068edff2095c7e9981e56077b3
Razi Ahmad <mail2razi4u@gmail.com>
2023-04-09 13:30:22 +0530

    small change

--- application/views/users/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php $this->load->view('users/head'); ?>
-						<div class="mainContent DashboardContent">
-							
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-<?php $this->load->view('users/footer'); ?>
--- application/views/users/footer.php
+++ application/views/users/footer.php
@@ -34,11 +34,10 @@
 	</div>
  
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<!--<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>-->
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.tutorialjinni.com/intl-tel-input/17.0.19/js/intlTelInput.min.js"></script>
-  <script src="<?php echo base_url();?>assets/js/custom.js"></script>
+  <script src="<?php echo base_url();?>assets/main/js/custom.js"></script>
 
   
 	<script>
